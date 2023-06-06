bb7c193f17850077301f5e3720dc7d8be81f3983
Toastalot <115156735+toastalot@users.noreply.github.com>
2023-06-06 09:22:27 +0100

    collapse hamburger menu when link clicked

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-document.querySelector(".hamburger").addEventListener("click", () => {
-	document.querySelector(".navbar-links").classList.toggle("active");
-});
-
+const hamburger = document.querySelector(".hamburger");
+const navList = document.querySelector(".navbar-links");
 const navLinks = document.querySelectorAll(".navbar-links li a");
+
+hamburger.addEventListener("click", () => {
+	navList.classList.toggle("active");
+});
 
 navLinks.forEach((navLink) => {
 	navLink.addEventListener("click", () => handleClick(navLink));
@@ -12,4 +14,11 @@ function handleClick(link) {
 	navLinks.forEach((navLink) => {
 		navLink.classList.toggle("active", navLink === link);
 	});
+	if (
+		window.getComputedStyle(hamburger).getPropertyValue("display") === "block"
+	) {
+		setTimeout(() => {
+			navList.classList.toggle("active");
+		}, 150);
+	}
 }
